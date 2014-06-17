14d2520160979ebf19e90bb2dea3f6e749a675d5
kleinab <kleinab@mit.edu>
2014-06-17 15:44:51 -0400

    Removed results page

--- pollingapp/polls/views.py
+++ pollingapp/polls/views.py
@@ -12,10 +12,6 @@ def detail(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll': poll})
 
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
-    
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     try:
@@ -32,4 +28,4 @@ def vote(request, poll_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('polls:index'))
